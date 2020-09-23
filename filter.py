ab5b61fe4296cf92d4206175f98a4dfb36d73e65
Kapiyva <ri9.py1201@gmail.com>
2020-09-23 20:55:57 +0900

    フィルタ処理

--- filter.py
+++ filter.py
@@ -1,8 +1,9 @@
 import os
-import cv2
+import cv2 as cv
 import numpy as np
 
 def main():
+    img_mask = cv.imread("mask.png", cv.IMREAD_GRAYSCALE)
     # dataType = input("'part'か'problem'を入力\n")
     dataType = 'problem'
     if dataType == "part":
@@ -25,23 +26,39 @@ def main():
         if ext != ".bmp":
             continue
 
-        img_bgr = cv2.imread(file)
-        img_gray = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2GRAY)
+        img_gray = cv.imread(file, cv.IMREAD_GRAYSCALE)
 
-        kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]], np.float32)
-        img_fil = cv2.filter2D(img_gray, -1, kernel)
-        cv2.imwrite("{0}.tif".format(fName), img_fil)
+        _, img_fil = cv.threshold(img_gray, 63, 0, cv.THRESH_TOZERO)
+        # img_fil = cv.adaptiveThreshold(img_fil, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY_INV, 11, 0)
+        img_edge = cv.Canny(img_gray, 3, 15)
+        img_fil = cv.subtract(img_fil, img_edge)
+        # kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]], np.float32)
+        kernel = np.ones((5,5),np.uint8)
+        itr = 2
+        # 縮小
+        img_fil = cv.erode(img_fil, kernel, iterations=itr)
+        # 拡大
+        img_fil = cv.dilate(img_fil, kernel, iterations=itr-1)
+        # オープニング
+        img_fil = cv.morphologyEx(img_fil, cv.MORPH_OPEN, kernel)
+        # クロージング
+        img_fil = cv.morphologyEx(img_fil, cv.MORPH_CLOSE, kernel)
+        # バケツのマスク処理
+        # img_fil = cv.bitwise_and(img_fil, img_mask)
+        cv.imwrite("{0}.tif".format(fName), img_fil)
+        # cv.imwrite("{0}_edge.tif".format(fName), img_edge)
 
-def test():
-    file = 'Problem01_0162.bmp'
-    fName, _ = os.path.splitext(file)
-    img_bgr = cv2.imread(file)
-    img_gray = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2GRAY)
+# def test():
+    # file = 'mask.png'
+    # fName, _ = os.path.splitext(file)
+    # img_bgr = cv.imread(file)
+    # img_gray = cv.cvtColor(img_bgr, cv.COLOR_BGR2GRAY)
 
-    kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]], np.float32)
-    # img_blur = cv2.blur(img_gray,(5,5))
-    img_blur = cv2.filter2D(img_gray, -1, kernel)
-    cv2.imwrite("{0}.tif".format(fName), img_blur)
+    # img = cv.bitwise_not(img_gray)
+    # # kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]], np.float32)
+    # # # img_blur = cv.blur(img_gray,(5,5))
+    # # img_blur = cv.filter2D(img_gray, -1, kernel)
+    # cv.imwrite("{0}.png".format(fName), img)
 
 if __name__ == "__main__":
     main()
