d7b1a1f11a956bb565c2bea42aae2ed306ff2bcd
Kapiyva <ri9.py1201@gmail.com>
2020-09-23 13:49:23 +0900

    フィルタ処理の追加

--- filter.py
+++ filter.py
@@ -1,14 +1,17 @@
 import os
 import cv2
+import numpy as np
 
 def main():
-    dataType = input("'part'か'problem'を入力\n")
+    # dataType = input("'part'か'problem'を入力\n")
+    dataType = 'problem'
     if dataType == "part":
         dataNum = input("01~31を入力\n")
         dataName = "Part{0}".format(dataNum)
         os.chdir("dataset/Parts/" + dataName)
     elif dataType == "problem":
-        dataNum = input("01~05を入力\n")
+        # dataNum = input("01~05を入力\n")
+        dataNum = '01'
         dataName = "dataset/Problem{0}".format(dataNum)
         os.chdir(dataName)
     else:
@@ -24,17 +27,22 @@ def main():
 
         img_bgr = cv2.imread(file)
         img_gray = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2GRAY)
-        img_edge = cv2.Canny(img_gray, 0, 12)
-        new_image = cv2.addWeighted(img_gray, 1, img_edge, -1, 0)
-        cv2.imwrite("{0}.tif".format(fName), new_image)
+
+        kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]], np.float32)
+        img_fil = cv2.filter2D(img_gray, -1, kernel)
+        cv2.imwrite("{0}.tif".format(fName), img_fil)
 
 def test():
-    file = "dataset/Problem01/Problem01_0150"
-    img_bgr = cv2.imread("{0}.bmp".format(file))
+    file = 'Problem01_0162.bmp'
+    fName, _ = os.path.splitext(file)
+    img_bgr = cv2.imread(file)
     img_gray = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2GRAY)
-    img_edge = cv2.Canny(img_gray, 0, 12)
-    cv2.imwrite("{0}.tif".format(file), img_edge)
-    
+
+    kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]], np.float32)
+    # img_blur = cv2.blur(img_gray,(5,5))
+    img_blur = cv2.filter2D(img_gray, -1, kernel)
+    cv2.imwrite("{0}.tif".format(fName), img_blur)
+
 if __name__ == "__main__":
     main()
     # test()
