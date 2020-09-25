0a26f4f7dc7e72861468420076033601cbd92abd
Kapiyva <ri9.py1201@gmail.com>
2020-09-26 00:31:34 +0900

    エッジ適用を最後に

--- labeling.py
+++ labeling.py
@@ -4,12 +4,20 @@ import numpy as np
 
 
 def labeling(voxel, connect=6):
+    d, h, w = voxel.shape()
     connectivity = connect
     labeled_voxel = cc3d.connected_components(voxel, connectivity=connectivity)
 
-    # 領域数を出力
-    # numbers = np.max(labeled_voxel) - 1
-    # print('総領域数は' + numbers + 'つです')
+    label_count = [0] * np.amax(labeled_voxel)
+    thresh_size = 90 //
+    # 体積が一定以下のものを除去
+    for i in range(d):
+        for j in range(h):
+            for k in range(w):
+                label_count[voxel[i][j][k]] += 1
+
+    for i in range(np.amax(labeled_voxel)):
+        if label_count[i]
 
     return labeled_voxel
 
