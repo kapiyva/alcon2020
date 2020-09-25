13a8859e6850b517595cd21230e79c8da598c820
Kapiyva <ri9.py1201@gmail.com>
2020-09-26 01:01:00 +0900

    buf fix

--- labeling.py
+++ labeling.py
@@ -4,12 +4,15 @@ import numpy as np
 
 
 def labeling(voxel, connect=6):
-    d, h, w = voxel.shape()
+    d = voxel.shape[0]
+    h = voxel.shape[1]
+    w = voxel.shape[2]
     connectivity = connect
     labeled_voxel = cc3d.connected_components(voxel, connectivity=connectivity)
 
     label_count = [0] * np.amax(labeled_voxel)
     thresh_size = 90 // (0.064 ** 3)
+    cutted_label_num = 0
     # 体積が一定以下のものを除去
     for i in range(d):
         for j in range(h):
@@ -19,6 +22,9 @@ def labeling(voxel, connect=6):
     for i in range(np.amax(labeled_voxel)):
         if label_count[i] < thresh_size:
             labeled_voxel[labeled_voxel == i] = 0
+            cutted_label_num += 1
+
+    print("label num: {0}".format(np.amax(labeled_voxel) - cutted_label_num))
 
     return labeled_voxel
 
